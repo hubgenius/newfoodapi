8c26f0ac263eedfd567e00b2977a1ccc1385357e
geniushub <prajapatig2411@gmail.com>
2022-03-26 18:05:49 +0530

    newapi

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const app=express();
+const router = express.Router();
+const  morgan = require("morgan");
+const cors=require("cors")
+const bodyParser = require('body-parser');
+const appRoutes = require("./route/api")(router);
+app.use('/upload',express.static('uploads'));
+
+const mongoose = require('mongoose')
+const DB ="mongodb+srv://mern:kju5566@cluster0.ql0e8.mongodb.net/genius?retryWrites=true&w=majority";
+mongoose.connect(DB)
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false}));
+
+
+app.use('/',appRoutes);
+
+app.listen(8000,()=>console.log("succsessfull"));
