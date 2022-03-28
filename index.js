d2b07ebb62d40e2cc5d9e36472d4d84a990f9da1
geniushub <prajapatig2411@gmail.com>
2022-03-28 17:08:11 +0530

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const  port = process.env.port || 8000;
+const PORT = process.env.PORT || 8080;
 const app=express();
 const router = express.Router();
 const  morgan = require("morgan");
@@ -22,4 +22,4 @@ app.get('/', (req, res) => {
     res.send("Working");
 })
 
-app.listen(port,()=>console.log("succsessfull"));
+app.listen(PORT,()=>console.log("succsessfull", PORT));
